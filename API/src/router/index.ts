1b95b2a8724b71447539b395b5df0b33ff053ac5
neofitoEOF <lorenconi16@gmail.com>
2022-06-05 18:51:27 -0300

    feat: Add middwares para usuarios autorizados

--- API/src/router/index.ts
+++ API/src/router/index.ts
@@ -4,22 +4,24 @@ import { listControllerUsers } from "../useCases/ListRegister";
 import { editControllerRegister } from "../useCases/EditRegister";
 import { deleteRegisterController } from "../useCases/DeleteRegister";
 import { addControllerMaster } from "../useCases/AddRegisterMaster";
+import { checkJwt } from "../middlewares/checkJwt";
+
 const router = Router();
 
 router.post("/register", (request, response) => {
   return addControllerMaster.handle(request, response);
 });
 
-router.post("/register/student", (request, response) => {
+router.post("/register/student", checkJwt, (request, response) => {
   return addControllerRegister.handle(request, response);
 });
-router.get("/listUser/:cpf?", (request, response) => {
+router.get("/listUser/:cpf?", checkJwt, (request, response) => {
   return listControllerUsers.handle(request, response);
 });
-router.put("/editRegister", (request, response) => {
+router.put("/editRegister", checkJwt, (request, response) => {
   return editControllerRegister.handle(request, response);
 });
-router.delete("/deleteRegister/:cpf?", (request, response) => {
+router.delete("/deleteRegister/:cpf?", checkJwt, (request, response) => {
   return deleteRegisterController.handle(request, response);
 });
 export { router };
