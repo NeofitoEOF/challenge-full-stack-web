3d5d96a946d375064cd31c991f0b2b7a637bc0e5
neofitoEOF <lorenconi16@gmail.com>
2022-06-06 22:36:33 -0300

    feat: Add resposta do jwt para front futuramente consumir

--- API/src/useCases/Login/LoginController.ts
+++ API/src/useCases/Login/LoginController.ts
@@ -4,14 +4,14 @@ import { LoginUseUseCase } from "./LoginUseUseCase";
 export class LoginControllerUsers {
   constructor(private loginUseUseCase: LoginUseUseCase) {}
 
-  async handle(request: Request, response: Response): Promise<Response> {
+  async handle(request: Request, response: Response) {
     const { email, password } = request.body;
     try {
-      await this.loginUseUseCase.execute({
+      const token = await this.loginUseUseCase.execute({
         Email: email,
         Password: password,
       });
-      return response.status(201).send();
+      return response.status(201).json({ token });
     } catch (error) {
       return response
         .status(400)
