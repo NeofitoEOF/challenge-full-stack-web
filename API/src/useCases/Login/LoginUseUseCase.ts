b9c06b09355b3bce75bc113c2ac499b0c12cdad3
neofitoEOF <lorenconi16@gmail.com>
2022-06-06 22:36:02 -0300

    feat: Add token do jwt

--- API/src/useCases/Login/LoginUseUseCase.ts
+++ API/src/useCases/Login/LoginUseUseCase.ts
@@ -1,14 +1,23 @@
-import { ILoginDTO, ILoginResposta } from "./interface/ILoginDTO";
+import { ILoginDTO } from "./interface/ILoginDTO";
 import { register } from "../../repository/ormUtilizado";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { auth } from "../../config/Auth";
 export class LoginUseUseCase {
-  async execute({ Email, Password }: ILoginDTO): Promise<ILoginResposta> {
-    if (Email === null || Email === "")
-      throw new Error("Unable to verify a blank email");
-    if (Password === null || Password === "")
-      throw new Error("Unable to verify a blank user");
+  async execute(data: ILoginDTO): Promise<string> {
+    if (!data.Email) throw new Error("Unable to verify a blank email");
+    if (!data.Password) throw new Error("Unable to verify a blank user");
     try {
-      const verifyEmail = await register.findByEmail(Email as unknown as ILoginDTO);
-      return verifyEmail;
+      const verifyData = await register.findByEmail(data);
+      if (!verifyData) throw new Error("Email alread not exists");
+
+      if (!(await bcrypt.compare(String(data.Password), verifyData.password)))
+        throw new Error("Password is incorrect");
+
+      const token = jwt.sign({ id: verifyData.id }, auth.secret, {
+        expiresIn: auth.expires,
+      });
+      return token;
     } catch (error) {
       throw new Error(error.message);
     }
