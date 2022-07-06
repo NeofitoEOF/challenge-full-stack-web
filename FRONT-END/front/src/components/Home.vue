8d9b232834197cd24dbb6d1b565937d0bc7ca679
neofitoEOF <lorenconi16@gmail.com>
2022-07-05 22:44:37 -0300

    fix: Aplicando correção para aplicação não quebrar

--- FRONT-END/front/src/components/Home.vue
+++ FRONT-END/front/src/components/Home.vue
@@ -12,7 +12,7 @@
       <v-btn text @click="showHome()">
         <v-icon>mdi-home</v-icon>
       </v-btn>
-      <v-btn text @click="showStudentsComponent()"> Aluno </v-btn>
+      <v-btn text @click="showStudentsComponent()"> Admin </v-btn>
     </v-app-bar>
     <div v-show="home">
       <p class="text-center headline pa-16">Bem vindo ao Módulo Acadêmico</p>
@@ -23,55 +23,20 @@
 
 <script>
 export default {
-  name: "StudentComponent",
+  name: "HelloWorld",
 
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Roadmap",
-        href: "https://vuetifyjs.com/introduction/roadmap/",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
+    studentComponent: false,
+    home: true,
   }),
+
+  methods: {
+    showHome() {
+      (this.studentComponent = false), (this.home = true);
+    },
+    showStudentsComponent() {
+      (this.home = false), (this.studentComponent = true);
+    },
+  },
 };
 </script>
