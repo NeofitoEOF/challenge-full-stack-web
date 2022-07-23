962e89720ba23394642cca23af68a90625492fe1
neofitoEOF <lorenconi16@gmail.com>
2022-07-23 16:58:44 -0300

    feat(loginUseUseCase): correção do Auth para auth

--- API/src/shared/useCases/Login/LoginUseUseCase.ts
+++ API/src/shared/useCases/Login/LoginUseUseCase.ts
@@ -1,7 +1,7 @@
 import { ILoginDTO } from "./interface/ILoginDTO";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { Auth } from "../../config/Auth/Index";
+import { auth } from "../../config/Auth/Index";
 import { email } from "../../repository/FindByEmail";
 import { ILogin } from "./interface/ILogin";
 export class LoginUseUseCase implements ILogin {
@@ -15,8 +15,8 @@ export class LoginUseUseCase implements ILogin {
       if (!(await bcrypt.compare(String(data.Password), verifyData.password)))
         throw new Error("Password is incorrect");
 
-      const token = jwt.sign({ id: verifyData.id }, Auth.secret, {
-        expiresIn: Auth.expires,
+      const token = jwt.sign({ id: verifyData.id }, auth.secret, {
+        expiresIn: auth.expires,
       });
       return token;
     } catch (error) {
