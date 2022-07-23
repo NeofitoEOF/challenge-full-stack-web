e2ad1a5d31eb0c26f1d314cf2b2ae4ad475444f3
neofitoEOF <lorenconi16@gmail.com>
2022-07-23 16:26:21 -0300

    feat(server): Removendo swagger não foi aplicado

--- API/src/api/server.ts
+++ API/src/api/server.ts
@@ -3,10 +3,6 @@ import { router } from "./router";
 
 const app = express();
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger_output.json");
-
 app.use(express.json());
 app.use(router);
-app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.listen(3000, () => console.log("Server running http://localhost:3000"));
